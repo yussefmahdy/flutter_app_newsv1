5d9be954ba7fc808a331ae43ce4cc1a75cdcc566
yussefmahdy <yuamahdy@gmail.com>
2021-06-15 16:00:37 +0300

    app

--- lib/Modules/Social-app/Social-app-cubits/Social-cubits.dart
+++ lib/Modules/Social-app/Social-app-cubits/Social-cubits.dart
@@ -168,7 +168,7 @@ class SocialCubit extends Cubit<SocialStates> {
       codeSent: (String verificationId, int resendToken) async {
         emit(CodeSentSuccessState());
         // Update the UI - wait for the user to enter the SMS code
-        String smsCode = "hi dad i am talking to you on firebase";
+        String smsCode = "xxxx";
 
         // Create a PhoneAuthCredential with the code
         PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: smsCode);
